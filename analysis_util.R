b3860260c5fa4253531b2103c40c021cad7ec421
akaring <anne.karing@gmail.com>
2017-03-06 07:38:25 -0800

    Within HH preferences calendar. A few duplicated regressions with restricted/covar sample. Comments.

--- analysis_util.R
+++ analysis_util.R
@@ -61,7 +61,7 @@ prepare.analysis.data <- function(.census.data, .takeup.data, .endline.data, .co
            baseline.sample = !is.na(baseline.sample.wave),
            gender = factor(gender, levels = 1:2, labels = c("male", "female"))) %>% 
     left_join(select(.endline.data, KEY.individ, age, school, floor, ethnicity, any.sms.reported, gift_choice,
-                     hh_cal, cal_value), "KEY.individ") %>% 
+                     hh_cal, cal_value, hh_bracelet, number_bracelet), "KEY.individ") %>% 
     left_join(select(.cluster.strat.data, wave, county, cluster.id, dist.pot.group), c("wave", "county", "cluster.id")) %>% 
     `attr<-`("class", c("takeup_df", class(.)))
 }
