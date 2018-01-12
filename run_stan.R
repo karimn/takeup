698af839090956c9d5d9a05f5de9a9233ee8656e
Karim Naguib <karim.naguib@evidenceaction.org>
2018-01-12 06:55:50 -0500

    Latest run with SMS treatments
    Name of model files is actually in accurately with "sms_ctrl"

--- run_stan.R
+++ run_stan.R
@@ -182,16 +182,20 @@ assertthat::assert_that(all(map_lgl(dyn_all_ate, ~ !any(is.na(.)))))
 
 # Analysis Data -----------------------------------------------------------
 
+
 dyn_analysis_data <- analysis.data %>%
   mutate(private_value = fct_collapse(assigned.treatment, 
                                       control = c("control", "ink"), 
                                       "calendar" = c("calendar", "bracelet")), 
          social_value = fct_collapse(assigned.treatment, control = c("control", "calendar")),
          sms.treatment.2 = fct_recode(sms.treatment.2, control = "sms.control")) %>% 
-  filter(!name_matched, sms.treatment.2 == "control") 
+  filter(!name_matched) #, sms.treatment.2 == "control") #, !hh.baseline.sample)
 
 dyn_static_treatment_map <- dyn_analysis_data %>% 
-  data_grid(private_value, social_value, dist.pot.group, phone_owner) %>% 
+  data_grid(private_value, social_value, sms.treatment.2, dist.pot.group, phone_owner) %>% 
+  filter(sms.treatment.2 == "control" | phone_owner,
+         sms.treatment.2 != "reminder.only" | (private_value == "control" & social_value == "control"),
+         private_value == "control" | social_value != "ink") %>%
   prepare_treatment_map()
 
 dyn_stan_data <- prepare_bayesian_analysis_data(
@@ -201,17 +205,18 @@ dyn_stan_data <- prepare_bayesian_analysis_data(
   prepared_treatment_maps = TRUE, 
   treatment_map = dyn_static_treatment_map,
     
-  treatment_formula = ~ (private_value + social_value * dist.pot.group) * phone_owner,
+  # treatment_formula = ~ (private_value + social_value * dist.pot.group) * phone_owner,
+  treatment_formula = ~ (private_value + social_value * dist.pot.group) * phone_owner + (social_value * dist.pot.group) : sms.treatment.2 + sms.treatment.2,
   subgroup_col = "phone_owner",
   drop_intercept_from_dm = FALSE, 
   nonparam_dynamics = TRUE,
   param_poly_order = 2,
   
-  all_ate = dyn_all_ate %>% 
-    filter(sms.treatment.2_left == "control",
-           sms.treatment.2_right == "control") %>% 
-    select(-starts_with("sms.treatment"), -starts_with("reminder_info_stock")) %>% 
-    distinct(),
+  all_ate = dyn_all_ate,
+    # filter(sms.treatment.2_left == "control",
+    #        sms.treatment.2_right == "control") %>% 
+    # select(-starts_with("sms.treatment"), -starts_with("reminder_info_stock")) %>% 
+    # distinct(),
   
   scale_sigma = 1,
   hyper_coef_sigma = 1,
@@ -226,12 +231,14 @@ dyn_stan_data <- prepare_bayesian_analysis_data(
 
 # Run ---------------------------------------------------------------------
 
-dyn_fit_version <- "param_sms_ctrl_3"
+dyn_fit_version <- "param_sms_ctrl_6"
 
 model_3_param <- stan_model(file = file.path("stan_models", "takeup_model_3_param.stan"), model_name = "model_3_param")
 
 model_3_fit <- dyn_stan_data %>% 
   sampling(model_3_param, data = ., 
-           chains = 4, iter = 2000,
+           chains = 4, iter = 600,
            control = lst(max_treedepth = 15), #adapt_delta = 0.99), 
            sample_file = file.path("stanfit", str_interp("model_3_${dyn_fit_version}.csv")))
+
+save(param_dyn_stan_data, file = file.path("stan_analysis_data", str_interp("model_3_${dyn_fit_version}.RData")))
