652b8e52b93ced6d8787dce8e34b1d5f5213f512
Karim Naguib <karim.naguib@evidenceaction.org>
2018-04-30 12:18:05 -0400

    Fix slurm scripts for census covar usage

--- slurm_scripts/takeup_slurm_batch_static.sh
+++ slurm_scripts/takeup_slurm_batch_static.sh
@@ -13,4 +13,4 @@
 
 module load r 
 
-Rscript ~/Code/dewormtheworld/takeup/run_stan.R static --num-chains=8 --num-iterations=600 --adapt-delta=0.9 --max-treedepth=25 --include-name-matched --use-census-covar --output-name=param_static_$SLURM_JOB_ID --output-dir=/global/scratch/knaguib 
+Rscript ~/Code/dewormtheworld/takeup/run_stan.R static --num-chains=8 --num-iterations=600 --adapt-delta=0.9 --max-treedepth=25 --include-name-matched --output-name=param_static_$SLURM_JOB_ID --output-dir=/global/scratch/knaguib 
