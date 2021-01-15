73e74f104c5c9c42b41bcc0e0984aea4c5560327
Karim Naguib <karimn2.0@gmail.com>
2021-01-15 10:33:27 -0500

    Change MEM and time

--- takeup_sim_slurm.sh
+++ takeup_sim_slurm.sh
@@ -4,11 +4,13 @@
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=24       # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=15G        # memory per cpu-core (4G is default)
-#SBATCH --time=2-06:00:00        # maximum time needed (HH:MM:SS)
+#SBATCH --mem-per-cpu=5G         # memory per cpu-core (4G is default)
+#SBATCH --time=3-00:00:00        # maximum time needed (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-user=karimn2.0@gmail.com
+#SBATCH --output=temp/log/takeup_sim-%j.log
+#SBATCH --error=temp/log/takeup_sim-%j.log
 
 module purge
 module load rh/devtoolset/8 gdal
