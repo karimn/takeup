20c802ff14172182d59665226464d050dc35b29b
Karim Naguib <karimn2.0@gmail.com>
2021-02-05 21:08:33 -0500

    Update args

--- all_run.sh
+++ all_run.sh
@@ -3,11 +3,11 @@
 VERSION=30
 CMDSTAN_ARGS="--cmdstanr --include-paths=~/Code/takeup/stan_models"
 # MODELS="--models=1,3,4"
-MODELS="--models=STRUCTURAL_LINEAR"
+MODELS="--models=REDUCED_FORM_NO_RESTRICT"
 
 ./run_stan_dist.R fit --outputname=dist_fit${VERSION} ${MODELS} ${CMDSTAN_ARGS} --update
 # ./run_stan_dist.R cv --outputname=dist_kfold${VERSION} --folds=10 ${MODELS} ${CMDSTAN_ARGS} --update
 # ./postprocess_dist_fit.R $VERSION 
 
 # Simulation
-./run_stan_dist_sim.R --cmdstanr --include-paths=~/Code/takeup/stan_models
+# ./run_stan_dist_sim.R ${CMDSTAN_ARGS} 
