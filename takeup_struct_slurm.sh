ac4e7930e746b35c1f6de9cb8d0a1e513208a125
Karim Naguib <karimn2.0@gmail.com>
2021-02-22 16:41:54 -0500

    Support runs similar to all_run.sh

--- takeup_struct_slurm.sh
+++ takeup_struct_slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=48       # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
-#SBATCH --time=0-06:00:00        # maximum time needed (HH:MM:SS)
+#SBATCH --time=0-03:00:00        # maximum time needed (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-user=karimn2.0@gmail.com
@@ -15,5 +15,11 @@
 module purge
 module load rh/devtoolset/8 gdal
 
-Rscript run_stan_dist.R fit --sequential --models=STRUCTURAL_LINEAR_U_SHOCKS --chains=4 --threads=12 --cmdstanr --include-paths=~/Code/takeup/stan_models --output-path=/tigress/kn6838/takeup --force-iter --iter=400
+VERSION=31
+CMDSTAN_ARGS="--cmdstanr --include-paths=~/Code/takeup/stan_models"
+MODELS="--models=STRUCTURAL_LINEAR_U_SHOCKS" # REDUCED_FORM_NO_RESTRICT
+
+# Rscript run_stan_dist.R fit --sequential --cmdstanr --include-paths=~/Code/takeup/stan_models --output-path=/tigress/kn6838/takeup --force-iter --iter=400
+
+Rscript run_stan_dist.R fit --sequential --chains=4 --threads=12 --outputname=dist_fit${VERSION} ${MODELS} ${CMDSTAN_ARGS} --output-path=/tigress/kn6838/takeup --update
 
