6c94ae06ccc49a19d27e66e416d199d54f6013c0
Karim Naguib <karimn2.0@gmail.com>
2021-03-30 10:28:04 -0400

    Version 41

--- takeup_struct_slurm.sh
+++ takeup_struct_slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=48       # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
-#SBATCH --time=0-8:00:00        # maximum time needed (HH:MM:SS)
+#SBATCH --time=0-6:00:00        # maximum time needed (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-user=karimn2.0@gmail.com
@@ -18,7 +18,7 @@ if [ -z ${IN_SLURM} ]; then
   module load rh/devtoolset/8 gdal
 fi
 
-VERSION=40
+VERSION=41
 CMDSTAN_ARGS="--cmdstanr --include-paths=~/Code/takeup/stan_models"
 MODELS="--models=STRUCTURAL_LINEAR_U_SHOCKS" # REDUCED_FORM_NO_RESTRICT
 SLURM_INOUT_DIR=/tigress/kn6838/takeup
@@ -27,5 +27,5 @@ NUM_STAN_THREADS=12
 Rscript run_takeup.R takeup prior --sequential --chains=4 --threads=${NUM_STAN_THREADS} --outputname=dist_prior${VERSION} ${MODELS} ${CMDSTAN_ARGS} --output-path=${SLURM_INOUT_DIR} --update 
 Rscript run_takeup.R takeup fit   --sequential --chains=4 --threads=${NUM_STAN_THREADS} --outputname=dist_fit${VERSION}   ${MODELS} ${CMDSTAN_ARGS} --output-path=${SLURM_INOUT_DIR} --update 
 
-Rscript postprocess_dist_fit.R ${VERSION} --cores=48 --input-path=${SLURM_INOUT_DIR} --output-path=${SLURM_INOUT_DIR} --load-from-csv --no-rate-of-change
+Rscript postprocess_dist_fit.R ${VERSION} --cores=48 --input-path=${SLURM_INOUT_DIR} --output-path=${SLURM_INOUT_DIR} --load-from-csv 
 
