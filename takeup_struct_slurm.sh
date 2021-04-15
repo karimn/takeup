0c0c49334c571d547605aa5022c6b5fa87b98e38
Karim Naguib <karimn2.0@gmail.com>
2021-04-15 16:05:53 -0400

    Adding multilevel support to full take-up model

--- takeup_struct_slurm.sh
+++ takeup_struct_slurm.sh
@@ -4,8 +4,8 @@
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=32       # cpu-cores per task (>1 if multi-threaded tasks)
-#BSATCH --mem=30G                # Total memory                     
-#SBATCH --time=0-8:00:00        # maximum time needed (HH:MM:SS)
+#SBATCH --mem=30G                # Total memory                     
+#SBATCH --time=0-12:00:00        # maximum time needed (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-user=karimn2.0@gmail.com
@@ -18,15 +18,15 @@ if [ -z ${IN_SLURM} ]; then
   module load rh/devtoolset/8 gdal
 fi
 
-VERSION=48
+VERSION=50
 CMDSTAN_ARGS="--cmdstanr --include-paths=~/Code/takeup/stan_models"
 MODELS="--models=STRUCTURAL_LINEAR_U_SHOCKS" # REDUCED_FORM_NO_RESTRICT
 SLURM_INOUT_DIR=/tigress/kn6838/takeup
 NUM_STAN_THREADS=8
 NUM_DIST_MIX=1
 
-Rscript run_takeup.R takeup prior --sequential --chains=4 --threads=${NUM_STAN_THREADS} --num-mix-groups=${NUM_DIST_MIX} --outputname=dist_prior${VERSION} ${MODELS} ${CMDSTAN_ARGS} --output-path=${SLURM_INOUT_DIR} --update 
-Rscript run_takeup.R takeup fit   --sequential --chains=4 --threads=${NUM_STAN_THREADS} --num-mix-groups=${NUM_DIST_MIX} --outputname=dist_fit${VERSION}   ${MODELS} ${CMDSTAN_ARGS} --output-path=${SLURM_INOUT_DIR} --update 
+Rscript run_takeup.R takeup prior --sequential --chains=4 --threads=${NUM_STAN_THREADS} --multilevel --num-mix-groups=${NUM_DIST_MIX} --outputname=dist_prior${VERSION} ${MODELS} ${CMDSTAN_ARGS} --output-path=${SLURM_INOUT_DIR} --update 
+Rscript run_takeup.R takeup fit   --sequential --chains=4 --threads=${NUM_STAN_THREADS} --multilevel --num-mix-groups=${NUM_DIST_MIX} --outputname=dist_fit${VERSION}   ${MODELS} ${CMDSTAN_ARGS} --output-path=${SLURM_INOUT_DIR} --update 
 
 Rscript postprocess_dist_fit.R ${VERSION} --cores=32 --input-path=${SLURM_INOUT_DIR} --output-path=${SLURM_INOUT_DIR} --load-from-csv 
 
