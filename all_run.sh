d3dc5e6cd3914944537d0385eb672075bba93cd7
Karim Naguib <karimn2.0@gmail.com>
2022-07-20 14:43:38 -0400

    Drop include-paths
    Default is stan_models now

--- all_run.sh
+++ all_run.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=24       # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=2G        # memory per cpu-core (4G is default)
+#SBATCH --mem-per-cpu=1G         # memory per cpu-core (4G is default)
 #SBATCH --time=0-06:00:00        # maximum time needed (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
@@ -16,7 +16,7 @@
 
 LATEST_VERSION=53
 VERSION=${1:-$LATEST_VERSION} # Get version from command line if provided
-CMDSTAN_ARGS="--cmdstanr --include-paths=stan_models"
+CMDSTAN_ARGS="--cmdstanr"
 SLURM_INOUT_DIR=~/scratch-midway2
 
 if [[ -v IN_SLURM ]]; then
@@ -39,18 +39,18 @@ fi
 STAN_THREADS=$((${CORES} / 4))
 
 # WTP 
-Rscript ./run_takeup.R wtp prior --chains=4 --iter 2000 --outputname=wtp_model_prior --output-path=data/stan_analysis_data --include-paths=stan_models --multilevel &
-Rscript ./run_takeup.R wtp fit   --chains=4 --iter 2000 --outputname=wtp_model_fit   --output-path=data/stan_analysis_data --include-paths=stan_models --multilevel
-wait
+# Rscript ./run_takeup.R wtp prior --chains=4 --iter 2000 --outputname=wtp_model_prior --output-path=data/stan_analysis_data --multilevel &
+# Rscript ./run_takeup.R wtp fit   --chains=4 --iter 2000 --outputname=wtp_model_fit   --output-path=data/stan_analysis_data --multilevel
+# wait
 
 # Distance
-#Rscript ./run_takeup.R dist prior --chains=4 --iter 800 --outputname=dist_model_prior --output-path=data/stan_analysis_data --include-paths=stan_models --num-mix-groups=1 --multilevel &
-#Rscript ./run_takeup.R dist fit   --chains=4 --iter 800 --outputname=dist_model_fit   --output-path=data/stan_analysis_data --include-paths=stan_models --num-mix-groups=1 --multilevel
+#Rscript ./run_takeup.R dist prior --chains=4 --iter 800 --outputname=dist_model_prior --output-path=data/stan_analysis_data --num-mix-groups=1 --multilevel &
+#Rscript ./run_takeup.R dist fit   --chains=4 --iter 800 --outputname=dist_model_fit   --output-path=data/stan_analysis_data --num-mix-groups=1 --multilevel
 #wait
 
 # Beliefs
-#Rscript ./run_takeup.R beliefs prior --chains=4 --iter 1000 --outputname=beliefs_prior --output-path=data/stan_analysis_data --include-paths=stan_models &
-#Rscript ./run_takeup.R beliefs fit   --chains=4 --iter 1000 --outputname=beliefs       --output-path=data/stan_analysis_data --include-paths=stan_models --num-mix-groups=1
+#Rscript ./run_takeup.R beliefs prior --chains=4 --iter 1000 --outputname=beliefs_prior --output-path=data/stan_analysis_data &
+#Rscript ./run_takeup.R beliefs fit   --chains=4 --iter 1000 --outputname=beliefs       --output-path=data/stan_analysis_data --num-mix-groups=1
 #wait
 
 # Reduced Form 
@@ -62,9 +62,10 @@ wait
 
 # Rscript ./run_takeup.R takeup prior --models=STRUCTURAL_LINEAR_U_SHOCKS ${CMDSTAN_ARGS} ${OUTPUT_ARGS} --threads=${STAN_THREADS} --update --outputname=dist_prior${VERSION} --num-mix-groups=1 --sequential
 # Rscript ./run_takeup.R takeup fit   --models=STRUCTURAL_LINEAR_U_SHOCKS ${CMDSTAN_ARGS} ${OUTPUT_ARGS} --threads=${STAN_THREADS} --update --outputname=dist_fit${VERSION}   --num-mix-groups=1 --sequential
+Rscript ./run_takeup.R takeup fit   --models=STRUCTURAL_LINEAR_U_SHOCKS_NO_SUBMODELS ${CMDSTAN_ARGS} ${OUTPUT_ARGS} --threads=${STAN_THREADS} --update --outputname=dist_fit${VERSION}   --num-mix-groups=1 --sequential
 # Rscript ./run_takeup.R takeup cv    --models=STRUCTURAL_LINEAR_U_SHOCKS ${CMDSTAN_ARGS} ${OUTPUT_ARGS}             --update --outputname=dist_kfold${VERSION} --num-mix-groups=1 --folds=10
 
-# Rscript ./postprocess_dist_fit.R ${VERSION} ${POSTPROCESS_INOUT_ARGS} --cores=1 --load-from-csv
+Rscript ./postprocess_dist_fit.R ${VERSION} ${POSTPROCESS_INOUT_ARGS} --cores=1 --load-from-csv
 
 # Simulation
 # ./run_stan_dist_sim.R ${CMDSTAN_ARGS} 
