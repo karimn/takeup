9a590f5b4cbf75a231d69db372ebfe35064c665c
Karim Naguib <karimn2.0@gmail.com>
2022-08-08 12:25:23 -0500

    Running version 58

--- postprocess.sh
+++ postprocess.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=40G        # memory per cpu-core (4G is default)
-#SBATCH --time=0-01:30:00        # maximum time needed (HH:MM:SS)
+#SBATCH --time=0-02:00:00        # maximum time needed (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-user=karimn2.0@gmail.com
@@ -14,7 +14,7 @@
 #SBATCH --error=temp/log/takeup_postprocess-%j.log
 #SBATCH --export=IN_SLURM=1
 
-LATEST_VERSION=54
+LATEST_VERSION=58
 VERSION=${1:-$LATEST_VERSION} # Get version from command line if provided
 CMDSTAN_ARGS="--cmdstanr"
 SLURM_INOUT_DIR=~/scratch-midway2
