56e11b74808e919d57a6ea4bd5d9a25486a163c9
EdJeeOnGitHub <edjee96@gmail.com>
2022-11-13 18:15:27 -0600

    belief breakdown added

--- scratch/belief-breakdown.R
+++ scratch/belief-breakdown.R
@@ -114,8 +114,8 @@ analysis_data %>%
         knows_other_dewormed_yes:doesnt_know_other_dewormed) %>%
     mutate(prop = value/obs_know_person) %>%
     filter(prop > 1)
-
-analysis_data %>%
+stop()
+comp_belief_data = analysis_data %>%
     filter(obs_know_person > 0)  %>%
     select(KEY.individ, contains("know"), assigned.treatment, dist.pot.group, assigned_dist_group) %>%
     mutate(
@@ -144,11 +144,22 @@ analysis_data %>%
     mutate(prop = value/obs_know_person) %>%
     group_by(assigned.treatment, assigned_dist_group, knowledge_type, belief_type) %>% 
     summarise(
-        estimate = mean(prop), 
-        std.error = sd(prop),
-        conf.low = estimate - 1.96*std.error, 
-        conf.high = estimate + 1.96*std.error
-    ) %>%
+        mean_est = mean(prop), 
+        std.error = sd(prop)/sqrt(n()),
+        per_0.5 = median(prop), 
+        per_0.05 = per_0.5 - cv(0.05)*std.error, 
+        per_0.95 = per_0.5 + cv(0.05)*std.error,
+        per_0.1 =  per_0.5 - cv(0.1)*std.error,
+        per_0.9 =  per_0.5 + cv(0.1)*std.error,
+        per_0.25 =  per_0.5 - cv(0.25)*std.error,
+        per_0.75 =  per_0.5 + cv(0.25)*std.error
+    )
+
+cv = function(alpha){
+  qnorm(1 - (alpha/2))
+}
+cv(0.05)
+comp_belief_data %>%
     # filter(assigned_dist_group == "close") %>%
     ggplot(
         aes(
@@ -167,4 +178,83 @@ analysis_data %>%
     labs(x = "Proportion", title = "Beliefs")
 
 ggsave("temp-data/prop-belief.png", width = 10, height = 10, dpi = 600)    
-    
+
+
+
+comp_belief_data %>%
+  filter(belief_type == "1ord") %>%
+  filter(assigned_dist_group == "close") %>%
+  ggplot(aes(
+    x = estimate, 
+    xmin = conf.low,
+    xmax = conf.high,
+    y = assigned.treatment, 
+    colour = knowledge_type
+  )) +
+  geom_pointrange(position = position_dodge(0.5)) +
+  theme_minimal() 
+
+
+pos_dodge = position_dodge(width = 0.3)
+crossbar_width = 0.2
+
+comp_belief_data %>%
+  filter(belief_type == "1ord") %>%
+  filter(assigned_dist_group == "close") %>%
+  ggplot(aes(y = assigned.treatment, group = knowledge_type)) +
+  geom_linerange(aes(xmin = per_0.05, xmax = per_0.95, color = knowledge_type), position = pos_dodge, size = 0.3) +
+  geom_crossbar(aes(x = per_0.5, xmin = per_0.1, xmax = per_0.9, color = knowledge_type), position = pos_dodge, fatten = 2, size = 0.4, width = crossbar_width) +
+  geom_linerange(aes(xmin = per_0.25, xmax = per_0.75, color = knowledge_type), position = pos_dodge, alpha = 0.4, size = 2.25) +
+  geom_point(aes(x = per_0.5, color = knowledge_type), position = pos_dodge, size = 1.8) +
+  geom_point(aes(x = per_0.5), position = pos_dodge, color = "white", size = 0.6) +
+  scale_y_discrete(drop = FALSE) +
+  scale_color_canva("", labels = "FOB", palette = canva_palette_vibrant) + 
+  labs(
+    title = "FOB",
+    subtitle = "",
+    x = "", y = "") +
+  theme(
+    legend.position = "bottom"
+  ) + 
+  NULL
+
+
+plot_single_beliefs_est <- function(beliefs_results_type_df, 
+                                    order, 
+                                    top_title = NULL, 
+                                    width = 0.3, 
+                                    crossbar_width = 0.2, 
+                                    vline = TRUE) {
+  pos_dodge <- position_dodge(width = width)
+  if (order == 1) {
+    str_title = "First Order Beliefs"
+  } else {
+    str_title = "Second Order Beliefs"
+  }
+  belief_plot = 
+      beliefs_results_type_df %>% 
+        filter(ord == order, assigned_dist_group_left == assigned_dist_group_right) %>% 
+        ggplot(aes(y = assigned_treatment_left, group = assigned_dist_group_left)) +
+        geom_linerange(aes(xmin = per_0.05, xmax = per_0.95, color = assigned_dist_group_left), position = pos_dodge, size = 0.3) +
+        geom_crossbar(aes(x = per_0.5, xmin = per_0.1, xmax = per_0.9, color = assigned_dist_group_left), position = pos_dodge, fatten = 2, size = 0.4, width = crossbar_width) +
+        geom_linerange(aes(xmin = per_0.25, xmax = per_0.75, color = assigned_dist_group_left), position = pos_dodge, alpha = 0.4, size = 2.25) +
+        geom_point(aes(x = per_0.5, color = assigned_dist_group_left), position = pos_dodge, size = 1.8) +
+        geom_point(aes(x = per_0.5), position = pos_dodge, color = "white", size = 0.6) +
+        scale_y_discrete(drop = FALSE) +
+        scale_color_canva("", labels = str_to_title, palette = canva_palette_vibrant) + 
+        labs(
+          title = str_title,
+          subtitle = "",
+          x = "", y = "") +
+        theme(
+          legend.position = "bottom"
+        ) + 
+        NULL
+
+  if (vline == TRUE) {
+    belief_plot = belief_plot +
+         geom_vline(xintercept = 0, linetype = "dotted") 
+  }
+        
+  return(belief_plot)
+} 
