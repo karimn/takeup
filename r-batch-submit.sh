a99b909175d93431fb4feb4d02b07959d7538824
EdJeeOnGitHub <edjee96@gmail.com>
2022-11-29 18:27:08 -0600

    swapped to multilevel

--- r-batch-submit.sh
+++ r-batch-submit.sh
@@ -5,7 +5,7 @@
 #PBS -l procs=30,mem=60gb
 cd $PBS_O_WORKDIR
 
-LATEST_VERSION=62
+LATEST_VERSION=70
 CMDSTAN_ARGS="--cmdstanr --include-paths=stan_models"
 VERSION=${1:-$LATEST_VERSION}
 OUTPUT_ARGS="--output-path=$PBS_O_WORKDIR"
@@ -23,4 +23,6 @@ mpirun -n 1 -machinefile $PBS_NODEFILE Rscript \
 	--update \
 	--outputname=dist_fit${VERSION} \
 	--num-mix-groups=1 \
-	--sequential > logs/outputfile.Rout 2>&1
+	--sequential \
+	--multilevel 
+	> logs/outputfile.Rout 2>&1
