e92145fc37f88fce81e1446c6a3f64725b4ead7a
Ed Jee <edjee96@gmail.com>
2022-12-17 14:15:08 -0600

    added switch for stan model type

--- stan_models/stan_owen_t.R
+++ stan_models/stan_owen_t.R
@@ -1,4 +1,14 @@
 library(rstan)
+if (stan_version() == "2.21.0") {
+stan_owen_t_code <-
+  '
+  functions {
+    real stan_owen_t(real x, real y) {
+      return owens_t(x, y);
+   }
+  }
+'
+} else {
 stan_owen_t_code <-
   '
   functions {
@@ -7,4 +17,5 @@ stan_owen_t_code <-
    }
   }
 '
+}
 expose_stan_functions(stanc(model_code = stan_owen_t_code))
