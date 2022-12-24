fbded13669c1ab6d32271f3b944cd2464e877cfe
Ed Jee <edjee96@gmail.com>
2022-12-24 01:52:33 -0600

    added cutoff arg

--- optim/run_optim.sh
+++ optim/run_optim.sh
@@ -8,9 +8,9 @@ NUMPOSTDRAWS=200
 SUBSIDY=0.25
 TARGETCONSTRAINT=0.32
 DRYRUN="" # "--sub-sample", "--fake-data"
-SKIPOPTIMISATION=""
-# SKIPOPTIMISATION="dont-skip-pls"
 VERSION=71
+POSTERIORMEDIAN="" # --posterior-median
+SKIPPREDICTION=1
 
 
 set -e
@@ -23,27 +23,36 @@ do
     else
         REPVAR="--suppress-rep"
     fi
-    echo $REPVAR
 
-    Rscript ./optim/predict-takeup-for-optim.R \
-                                ${VERSION} \
-                                --output-name=no-cutoff-${VARIABLE} \
-                                --from-csv \
-                                --num-post-draws=${NUMPOSTDRAWS} \
-                                --rep-cutoff=Inf \
-                                --dist-cutoff=10000 \
-                                --num-cores=12 \
-                                --type-lb=-Inf \
-                                --type-ub=Inf \
-                                --model=${MODEL} \
-                                ${PREDDISTANCE} \
-                                ${REPVAR} \
-                                bracelet \
-                                control
+    if [ ${POSTERIORMEDIAN} == "--posterior-median" ] 
+    then 
+        POSTVAR="median"
+    else
+        POSTVAR="post-draws"
+    fi
+    
+    if [ $SKIPPREDICTION != 1 ]
+    then
+        Rscript ./optim/predict-takeup-for-optim.R \
+                                    ${VERSION} \
+                                    --output-name=no-cutoff-${VARIABLE} \
+                                    --from-csv \
+                                    --num-post-draws=${NUMPOSTDRAWS} \
+                                    --rep-cutoff=Inf \
+                                    --dist-cutoff=10000 \
+                                    --num-cores=12 \
+                                    --type-lb=-Inf \
+                                    --type-ub=Inf \
+                                    --model=${MODEL} \
+                                    ${PREDDISTANCE} \
+                                    ${REPVAR} \
+                                    bracelet \
+                                    control
+    fi
 
 
     Rscript ./optim/optimal_allocation.R  \
-                                --posterior-median \
+                                ${POSTERIORMEDIAN} \
                                 --num-cores=12 \
                                 --min-cost  \
                                 --target-constraint=${TARGETCONSTRAINT} \
@@ -58,37 +67,41 @@ do
 
     Rscript ./optim/postprocess_allocation.R  \
                                 --min-cost \
-                                --posterior-median \
+                                ${POSTERIORMEDIAN} \
                                 --target-constraint=$TARGETCONSTRAINT \
                                 --input-path=optim/data \
-                                --optim-input-a-filename=no-cutoff-${VARIABLE}-median-optimal-allocation.rds \
+                                --optim-input-a-filename=no-cutoff-${VARIABLE}-${POSTVAR}-optimal-allocation.rds \
                                 --village-input-filename=village-df.csv \
                                 --pot-input-filename=pot-df.csv \
                                 --demand-input-a-filename=pred-demand-dist-fit${VERSION}-no-cutoff-${VARIABLE}.csv \
                                 --output-path=optim/plots \
-                                --output-basename=no-cutoff-${VARIABLE}-median \
-                                --map-plot
+                                --output-basename=no-cutoff-${VARIABLE}-${POSTVAR} \
+                                --map-plot \
+                                cutoff-type=no-cutoff
 
     ## now cutoff
-    Rscript ./optim/predict-takeup-for-optim.R \
-                                ${VERSION} \
-                                --output-name=cutoff-${VARIABLE} \
-                                --from-csv \
-                                --num-post-draws=${NUMPOSTDRAWS} \
-                                --rep-cutoff=Inf \
-                                --dist-cutoff=2500 \
-                                --num-cores=12 \
-                                --type-lb=-Inf \
-                                --type-ub=Inf \
-                                --model="STRUCTURAL_LINEAR_U_SHOCKS" \
-                                ${PREDDISTANCE} \
-                                ${REPVAR} \
-                                bracelet \
-                                control
+    if [ $SKIPPREDICTION != 1 ]
+    then
+        Rscript ./optim/predict-takeup-for-optim.R \
+                                    ${VERSION} \
+                                    --output-name=cutoff-${VARIABLE} \
+                                    --from-csv \
+                                    --num-post-draws=${NUMPOSTDRAWS} \
+                                    --rep-cutoff=Inf \
+                                    --dist-cutoff=2500 \
+                                    --num-cores=12 \
+                                    --type-lb=-Inf \
+                                    --type-ub=Inf \
+                                    --model="STRUCTURAL_LINEAR_U_SHOCKS" \
+                                    ${PREDDISTANCE} \
+                                    ${REPVAR} \
+                                    bracelet \
+                                    control
+    fi
 
 
     Rscript ./optim/optimal_allocation.R  \
-                                --posterior-median \
+                                ${POSTERIORMEDIAN} \
                                 --num-cores=12 \
                                 --min-cost  \
                                 --target-constraint=${TARGETCONSTRAINT} \
@@ -98,21 +111,22 @@ do
                                 --village-input-filename=village-df.csv \
                                 --pot-input-filename=pot-df.csv \
                                 --time-limit=10000 \
-                                --demand-input-filename=pred-demand-dist-fit${VERSION}-cutoff-${VARIABLE}.csv
+                                --demand-input-filename=pred-demand-dist-fit${VERSION}-cutoff-${VARIABLE}.csv 
 
 
     Rscript ./optim/postprocess_allocation.R  \
                                 --min-cost \
-                                --posterior-median \
+                                ${POSTERIORMEDIAN} \
                                 --target-constraint=$TARGETCONSTRAINT \
                                 --input-path=optim/data \
-                                --optim-input-a-filename=cutoff-${VARIABLE}-median-optimal-allocation.rds \
+                                --optim-input-a-filename=cutoff-${VARIABLE}-${POSTVAR}-optimal-allocation.rds \
                                 --village-input-filename=village-df.csv \
                                 --pot-input-filename=pot-df.csv \
                                 --demand-input-a-filename=pred-demand-dist-fit${VERSION}-cutoff-${VARIABLE}.csv \
                                 --output-path=optim/plots \
-                                --output-basename=cutoff-${VARIABLE}-median \
-                                --map-plot
+                                --output-basename=cutoff-${VARIABLE}-${POSTVAR} \
+                                --map-plot \
+                                cutoff-type=cutoff
 done
 
 
