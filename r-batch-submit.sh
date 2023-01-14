ab4bd3163249d4d6f326826018531c2df5507512
Ed Jee <edjee96@gmail.com>
2023-01-14 17:51:54 -0600

    added rbs file

--- r-batch-submit.sh
+++ r-batch-submit.sh
@@ -1,37 +1,86 @@
-#PBS -N "takeup-sms" 
+#PBS -N "takeup-all-run" 
 #PBS -j oe
 #PBS -V
 #PBS -e $PBS_O_WORKDIR/logs/qsub_log.txt
-#PBS -l procs=10,mem=40gb
+#PBS -l procs=16,mem=40gb
 cd $PBS_O_WORKDIR
 
-LATEST_VERSION=70
+LATEST_VERSION=73
 CMDSTAN_ARGS="--cmdstanr --include-paths=stan_models"
 VERSION=${1:-$LATEST_VERSION}
-OUTPUT_ARGS="--output-path=$PBS_O_WORKDIR"
+OUTPUT_ARGS="--output-path=$PBS_O_WORKDIR/data/stan_analysis_data"
+POSTPROCESS_INOUT_ARGS="--input_path=$PBS_O_WORKDIR/data/stan_analysis_data --output-path=$PBS_O_WORKDIR/temp-data"
+STAN_THREADS=3
 
-mpirun -n 1 -machinefile $PBS_NODEFILE Rscript \
-	--no-save \
-	--no-restore \
-	--verbose \
-	scratch/analyse-reduced-form-sms.R  \
-		--save-fit \
-		--fit-path=data/stan_analysis_data \
-		--fit-file=SMS_BRMS_reminder_fit.rds \
-		--include-reminder
 
-#mpirun -n 1 -machinefile $PBS_NODEFILE Rscript \
-#	--no-save \
-#	--no-restore \
-#	--verbose \
-#	run_takeup.R takeup fit \
-#	--models=STRUCTURAL_LINEAR_U_SHOCKS \
-#	${CMDSTAN_ARGS} \
-#	${OUTPUT_ARGS} \
-#	--threads=1 \
-#	--update \
-#	--outputname=dist_fit${VERSION} \
-#	--num-mix-groups=1 \
-#	--sequential \
-#	--multilevel 
-#	> logs/outputfile.Rout 2>&1
+
+
+mpirun -n 1 -machinefile $PBS_NODEFILE 
+	# Structural
+	Rscript \
+		--no-save \
+		--no-restore \
+		--verbose \
+		run_takeup.R takeup fit \
+		--models=STRUCTURAL_LINEAR_U_SHOCKS_NO_REP \
+		${CMDSTAN_ARGS} \
+		${OUTPUT_ARGS} \
+		--threads=1 \
+		--update \
+		--outputname=dist_fit${VERSION} \
+		--num-mix-groups=1 \
+		--sequential 
+		> logs/outputfile-structural-fit.Rout 2>&1 :
+	Rscript \
+		--no-save \
+		--no-restore \
+		--verbose \
+		run_takeup.R takeup prior \
+		--models=STRUCTURAL_LINEAR_U_SHOCKS_NO_REP \
+		${CMDSTAN_ARGS} \
+		${OUTPUT_ARGS} \
+		--threads=1 \
+		--update \
+		--outputname=dist_prior${VERSION} \
+		--num-mix-groups=1 \
+		--sequential 
+		> logs/outputfile-structural-prior.Rout 2>&1 :
+	# Reduced Form
+	Rscript \
+		--no-save \
+		--no-restore \
+		--verbose \
+		run_takeup.R takeup fit \
+		--models=REDUCED_FORM_NO_RESTRICT \
+		${CMDSTAN_ARGS} \
+		${OUTPUT_ARGS} \
+		--threads=1 \
+		--update \
+		--outputname=dist_fit${VERSION} \
+		--num-mix-groups=1 \
+		--sequential 
+		> logs/outputfile-reduced-fit.Rout 2>&1 :
+	Rscript \
+		--no-save \
+		--no-restore \
+		--verbose \
+		run_takeup.R takeup prior \
+		--models=REDUCED_FORM_NO_RESTRICT \
+		${CMDSTAN_ARGS} \
+		${OUTPUT_ARGS} \
+		--threads=1 \
+		--update \
+		--outputname=dist_prior${VERSION} \
+		--num-mix-groups=1 \
+		--sequential 
+		> logs/outputfile-reduced-prior.Rout 2>&1 :
+	# Postprocessing
+	Rscript \
+		--no-save \
+		--no-restore \
+		--verbose \
+		postprocess_dist_fit.R \
+		${VERSION} \
+		${POSTPROCESS_INOUT_ARGS} \ 
+		--cores=1 \ 
+		--load-from-csv
