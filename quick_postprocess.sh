ff37ebe0a1faa7f0043a760b6fba4b53d5842b7a
Ed Jee <edjee96@gmail.com>
2023-06-20 16:09:22 -0500

    maybe now in parallel

--- quick_postprocess.sh
+++ quick_postprocess.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --partition=bigmem2
 #SBATCH --job-name=quick-takeup        # create a short name for your job
-#SBATCH --nodes=2                # node count
+#SBATCH --nodes=1                # node count
 #SBATCH --ntasks=2              # total number of tasks across all nodes
-#SBATCH --cpus-per-task=8      # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --cpus-per-task=4      # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=10G         # memory per cpu-core (4G is default)
 #SBATCH --time=0-10:00:00        # maximum time needed (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
@@ -36,7 +36,6 @@ else
   CORES=8
 fi
 
-STAN_THREADS=$((${CORES} / 4))
 
 
 postprocess_struct_models () {
@@ -102,9 +101,10 @@ postprocess_rf_models () {
 
 }
 
+export -f postprocess_struct_models postprocess_rf_models
 
 
-srun --exclusive --ntasks=1 postprocess_struct_models "STRUCTURAL_LINEAR_U_SHOCKS_PHAT_MU_REP_STRATA_FOB" &
-srun --exclusive --ntasks=1 postprocess_struct_models "STRUCTURAL_LINEAR_U_SHOCKS_PHAT_MU_REP_FOB"	&
+
+srun --exclusive --ntasks=1 bash -c "postprocess_struct_models 'STRUCTURAL_LINEAR_U_SHOCKS_PHAT_MU_REP_STRATA_FOB'" &
+srun --exclusive --ntasks=1 bash -c "postprocess_struct_models 'STRUCTURAL_LINEAR_U_SHOCKS_PHAT_MU_REP_FOB'" &
 wait
-
