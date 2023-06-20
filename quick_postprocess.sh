c369a1cc9846875a85fb237c059f79cf64772eae
Ed Jee <edjee96@gmail.com>
2023-06-20 10:45:40 -0500

    added parallel hopefully

--- quick_postprocess.sh
+++ quick_postprocess.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-#SBATCH --partition=bigmem
-#SBATCH --job-name=takeup        # create a short name for your job
-#SBATCH --nodes=1                # node count
-#SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=12       # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --partition=bigmem2
+#SBATCH --job-name=quick-takeup        # create a short name for your job
+#SBATCH --nodes=2                # node count
+#SBATCH --ntasks=2              # total number of tasks across all nodes
+#SBATCH --cpus-per-task=8      # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=10G         # memory per cpu-core (4G is default)
 #SBATCH --time=0-10:00:00        # maximum time needed (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
@@ -38,8 +38,6 @@ fi
 
 STAN_THREADS=$((${CORES} / 4))
 
-
-postprocess_models () {
 	models=(
 		"STRUCTURAL_LINEAR_U_SHOCKS_PHAT_MU_REP_FOB" 
 		"REDUCED_FORM_NO_RESTRICT"
@@ -47,43 +45,51 @@ postprocess_models () {
 
 	for model in "${models[@]}"
 	do
-        Rscript --no-save \
-                --no-restore \
-                --verbose \
-                quick_ate_postprocess.R \
-                ${VERSION} \
-                ${POSTPROCESS_INOUT_ARGS} \
-                1 2 3 4
 
-        Rscript --no-save \
-                --no-restore \
-                --verbose \
-                quick_ate_postprocess.R \
-                ${VERSION} \
-                ${POSTPROCESS_INOUT_ARGS} \
-                --prior \
-                1 2 3 4
+postprocess_models () {
+    echo "RUNNING: $1"
+    Rscript --no-save \
+            --no-restore \
+            --verbose \
+            quick_ate_postprocess.R \
+            ${VERSION} \
+            ${POSTPROCESS_INOUT_ARGS} \
+            --model=$1 \
+            1 2 3 4
 
-        Rscript --no-save \
-                --no-restore \
-                --verbose \
-                quick_roc_postprocess.R \
-                ${VERSION} \
-                ${POSTPROCESS_INOUT_ARGS}
-                1 2 3 4
+    Rscript --no-save \
+            --no-restore \
+            --verbose \
+            quick_ate_postprocess.R \
+            ${VERSION} \
+            ${POSTPROCESS_INOUT_ARGS} \
+            --model=$1 \
+            --prior \
+            1 2 3 4
 
-        Rscript --no-save \
-                --no-restore \
-                --verbose \
-                quick_submodel_postprocess.R \
-                ${VERSION} \
-                ${POSTPROCESS_INOUT_ARGS}
-                1 2 3 4
-    done
+    Rscript --no-save \
+            --no-restore \
+            --verbose \
+            quick_roc_postprocess.R \
+            ${VERSION} \
+            ${POSTPROCESS_INOUT_ARGS} \
+            --model=$1 \
+            1 2 3 4
+
+    Rscript --no-save \
+            --no-restore \
+            --verbose \
+            quick_submodel_postprocess.R \
+            ${VERSION} \
+            ${POSTPROCESS_INOUT_ARGS} \
+            --model=$1 \
+            1 2 3 4
 }
 
 
-postprocess_models 
+srun -n1 postprocess_models "STRUCTURAL_LINEAR_U_SHOCKS_PHAT_MU_REP" &
+srun -n1 postprocess_models "REDUCED_FORM_NO_RESTRICT" &
+wait
 
 
 
