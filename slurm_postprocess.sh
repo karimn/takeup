740713b2b36b9ba29830997380183992e60c656e
= <=>
2023-06-21 06:46:08 -0500

    fixes

--- slurm_postprocess.sh
+++ slurm_postprocess.sh
@@ -16,8 +16,9 @@
 
 LATEST_VERSION=95
 VERSION=${1:-$LATEST_VERSION} # Get version from command line if provided
-CMDSTAN_ARGS="--cmdstanr"
 SLURM_INOUT_DIR=~/scratch-midway2
+MODEL="STRUCTURAL_LINEAR_U_SHOCKS_PHAT_MU_REP_FOB"
+PRIOR_ARG="" # "--prior"
 
 echo "Version: $VERSION"
 
@@ -27,25 +28,23 @@ if [[ -v IN_SLURM ]]; then
 
   module load -f midway2 gdal/2.4.1 udunits/2.2 proj/6.1 cmake R/4.2.0
   module load -f R/4.2.0
-
-  POSTPROCESS_INOUT_ARGS="--input-path=${SLURM_INOUT_DIR} --output-path=${SLURM_INOUT_DIR}"
+	IN_ARG="--input-path=${SLURM_INOUT_DIR}"
+	OUT_ARG="--output-path=${SLURM_INOUT_DIR}"
 
   echo "Running with ${CORES} cores."
-  echo "INOUT ARGS: ${POSTPROCESS_INOUT_ARGS}."
+  echo "INOUT ARGS: ${IN_ARG} ${OUT_ARG}."
 else
-  POSTPROCESS_INOUT_ARGS="--input-path=data/stan_analysis_data --output-path=temp-data/"
+	IN_ARG="--input-path=data/stan_analysis_data"
+	OUT_ARG="--output-path=temp-data"
   CORES=8
 fi
-
 
 # Source the functions script
 source quick_postprocess.sh
 
-# Rest of your submission script
-MODEL="STRUCTURAL_LINEAR_U_SHOCKS_PHAT_MU_REP_FOB"
 # Within SLURM tasks
-srun --export=all --exclusive --ntasks=1 bash -c "source quick_postprocess.sh && postprocess_model quick_ate_postprocess.R ${VERSION} ${POSTPROCESS_INOUT_ARGS} ${MODEL}" &
-srun --export=all --exclusive --ntasks=1 bash -c "source quick_postprocess.sh && postprocess_model quick_roc_postprocess.R ${VERSION} ${POSTPROCESS_INOUT_ARGS} ${MODEL}" &
-srun --export=all --exclusive --ntasks=1 bash -c "source quick_postprocess.sh && postprocess_model quick_submodel_postprocess.R ${VERSION} ${POSTPROCESS_INOUT_ARGS} ${MODEL}" &
+srun --export=all --exclusive --ntasks=1 bash -c "source quick_postprocess.sh && postprocess_model quick_ate_postprocess.R ${VERSION} ${MODEL} ${IN_ARG} ${OUT_ARG}" &
+srun --export=all --exclusive --ntasks=1 bash -c "source quick_postprocess.sh && postprocess_model quick_roc_postprocess.R ${VERSION} ${MODEL} ${IN_ARG} ${OUT_ARG}" &
+srun --export=all --exclusive --ntasks=1 bash -c "source quick_postprocess.sh && postprocess_model quick_submodel_postprocess.R ${VERSION} ${MODEL} ${IN_ARG} ${OUT_ARG}" &
 wait
 
